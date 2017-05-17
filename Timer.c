3d99b0787d1fbb8ad360e590d66bf576369d8b84
luisejrobles <luisejimenezrobles@gmail.com>
2017-05-16 20:27:10 -0700

    logica de isr lista

--- Timer.c
+++ Timer.c
@@ -11,10 +11,16 @@
 
 const struct note *cancion;
 static volatile uint8_t secFlag;
-static volatile uint8_t nota;
-static volatile uint8_t notaFin;
+static volatile uint8_t cntNota;
+static volatile uint8_t Fin;
 static volatile uint8_t volumen;
 static char volumenChar[20];
+static char freq[10];
+
+
+//BANDERAS ISR
+static volatile uint8_t notaFin = 0;
+static volatile uint8_t silFin = 0;
 
 
 void Timer0_Ini ( void ){
@@ -37,29 +43,36 @@ ISR(TIMER0_COMPA_vect){
 	/*	Agregar las instrucciones necesarias para reproducir
 		la siguiente nota en el arreglo dependiendo de la duración, 
 		e insertar los silencios entre cada nota. */
-	
-	mSeg++;
-	/*if( mSeg >= delay ){
+	if(mSeg <= cancion->delay)
+	{
+		Timer2_Freq_Gen(TICKS(cancion[cntNota].freq));	
+	}else
+	{
+		notaFin = 1;
 		mSeg = 0;
-		if(silencio && nota != notaFin){					//Si el indice es diferente al tam de la estructura
-			silencio = 0;									//apagar band , actualizar
-			Timer2_Freq_Gen(TICKS(*cancion[nota].freq));	//frec igual a notas x ticks.
-			delay = *cancion[nota].delay;					//El delay es la nota x 10ms
-			nota++;
-		}
-		else {												//Si el indice es igual al tam
-			silencio = 1;
-			Timer2_Freq_Gen(0);
-			delay = SILENCE;
-		}
-	}*/
+	}
+	if(notaFin && (mSeg <= SILENCE) )
+	{
+		Timer2_Freq_Gen(TICKS(0));		//mandando silencio
+	}else
+	{
+		silFin = 1;
+	}
+	
+	if(notaFin && silFin)
+	{
+		notaFin = 0;
+		silFin = 0;
+		cntNota ++;
+	}
 }
 
 void Timer2_Freq_Gen(uint8_t ticks){
 	/* 	Si "ticks" es mayor que 0 entonces, inicializa y habilita el Generador 
 		de Frecuencia del Timer2 con el tope dado por "ticks".
 		De lo contrario se requiere deshabilitar el Generador, generando de 
-		esta forma el silencio (0 lógico). */
+		esta forma el silencio (0 lógico).
+		*/
 		
 	if(ticks > 0)
 	{
@@ -68,39 +81,36 @@ void Timer2_Freq_Gen(uint8_t ticks){
 		OCR2A  = ticks -1 ;							 //Tope en OCR2A
 	}else
 	{
-		TCCR2B &= ~(6<<CS20);			//Deshabilitando PS para 0 logico
-	}
+		//TCCR2B &= ~(6<<CS20);			//Deshabilitando PS para 0 logico
+		TCCR2B = 0;
+	}	
 }
 
 void Timer2_Play(const struct note song[],unsigned int len)
 {	
 	/*	Función que establece las condiciones necesarias para que
 		el generador recorra el arreglo de notas. */
-	
-	cancion = &song[0];
-	nota = 0;				//obteniendo principio de musica
-	notaFin = len;			//Obteniendo final de musica
+	cancion = song;
+	cntNota = 0;				//obteniendo principio de musica
+	Fin = len;			//Obteniendo final de musica
 	volumen = 50;			//Volumen a la mitad
-	DDRH = (1<<DDH6);		//PB4 como salida
-	
-	UART0_puts(itoa(*cancion->delay));
 }
 
 void Timer2_Volume(int8_t direction){
 	/* 	Ajusta el ciclo de trabajo para incrementar o decrementar  el volumen
 	   	de las notas que se estan generando. */
-	
-	if(volumen <= 80)
+	if(direction == 1)
 	{
-		volumen += 10;
-	}
-	else if( volumen >= 10)
+		if(volumen <= 80)
+		{
+			volumen += 10;
+		}		
+	}else
 	{
-		volumen -= 10;	
+		if( volumen >= 10)
+		{
+			volumen -= 10;
+		}
 	}
-	
 	OCR2B = (OCR2A*volumen)/100;
-	itoa(volumenChar,OCR2B,10);
-	UART0_puts("\n\r\n\rVolumen: ");
-	UART0_puts(volumenChar);
 }
